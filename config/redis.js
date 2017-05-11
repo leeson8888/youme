240ff520b8923b6158c30289d3adb2675a8a33be
tseian <xieweicheng@maiqing-tech.com>
2017-05-11 17:39:07 +0800

    gitignore 优化

--- config/redis.js
+++ config/redis.js
@@ -3,8 +3,14 @@
  * 具体API参看  https://github.com/NodeRedis/node_redis
  */
 
+var bluebird = require('bluebird');
+
+
 var config = require("./config");
 var redis = require("redis"), client = redis.createClient(config.redis.port, config.redis.host, config.redis.options);
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
 client.on("error", function (err) {
     console.log(err);
