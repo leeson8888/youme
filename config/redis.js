5d94441df0aad9caa84c066019caaea377bb76ad
tseian <xieweicheng@maiqing-tech.com>
2017-05-11 17:16:10 +0800

    优化代码

--- config/redis.js
+++ config/redis.js
@@ -1,18 +1,10 @@
 /**
  * Created by tseian on 09/05/2017.
+ * 具体API参看  https://github.com/NodeRedis/node_redis
  */
 
 var config = require("./config");
 var redis = require("redis"), client = redis.createClient(config.redis.port, config.redis.host, config.redis.options);
-
-// node_redis donot require psw but password was suplied
-// client.auth(config.redis.psw, function (err) {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log("authenticate is success");
-// });
 
 client.on("error", function (err) {
     console.log(err);
